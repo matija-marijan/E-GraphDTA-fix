65fdc5deaf333b2574dd1e30af96f5fab34b897d
Matija Marijan <matijamarijan99@gmail.com>
2025-09-26 14:48:01 +0200

    Update validation bash script to accept num_layers for parameter tuning

--- run_validation.sh
+++ run_validation.sh
@@ -3,22 +3,25 @@ set -e
 trap "echo 'Stopping...'; kill 0" SIGINT
 
 datasets=("davis" "kiba")
-models=("GINConvNet" "ESM_GINConvNet" "FRI_GINConvNet" "PDC_GINConvNet" "Vnoc_GINConvNet" "PDC_Vnoc_GINConvNet" "ESM_GATNet" "GATNet" "GAT_GCN" "GCNNet")
+models=("ESM_GINConvNet" "FRI_GINConvNet" "PDC_GINConvNet" "Vnoc_GINConvNet" "PDC_Vnoc_GINConvNet")
 validation_folds=(0 1 2 3 4)
 seed=0
+num_layers=(1 2 3)
 
 for dataset in "${datasets[@]}"; do
     for model in "${models[@]}"; do
-        for fold in "${validation_folds[@]}"; do
-            python training.py --seed "$seed" --wandb --dataset "$dataset" --model "$model" --validation_fold "$fold"&
+        for layer in "${num_layers[@]}"; do
+            for fold in "${validation_folds[@]}"; do
+                python training.py --seed "$seed" --wandb --dataset "$dataset" --model "$model" --validation_fold "$fold" --num_layers "$layer"&
+            done
         done
         wait
     done
 done
 
-for model in "${models[@]}"; do
-    for fold in "${validation_folds[@]}"; do
-        python training.py --seed "$seed" --wandb --dataset "davis" --model "$model" --validation_fold "$fold" --mutation &
-    done
-    wait
-done
+# for model in "${models[@]}"; do
+#     for fold in "${validation_folds[@]}"; do
+#         python training.py --seed "$seed" --wandb --dataset "davis" --model "$model" --validation_fold "$fold" --mutation &
+#     done
+#     wait
+# done
